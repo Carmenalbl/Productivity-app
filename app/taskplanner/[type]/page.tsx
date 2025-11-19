00b455932aaaa210c0e67c2b2a8207c7ee318f4b
Carmenalbl <carmenalblopz@gmail.com>
2025-11-19 16:36:58 +0100

    add: enhance TaskPlanner component layout with improved styling and hover effects

--- app/taskplanner/[type]/page.tsx
+++ app/taskplanner/[type]/page.tsx
@@ -11,27 +11,66 @@ export default function TaskPlanner() {
         }}
       >
         {/* Columna izquierda dividida en 4 Box */}
-        <Box className="flex flex-col w-64 bg-gray-100 h-full"
+        <Box
+          className="flex flex-col w-64 bg-gray-100 h-full"
           sx={{ flex: 1, padding: 2 }}
         >
-          <Box className="mb-4 p-4 bg-white rounded shadow" sx={{ flex: 1 }}>
+          <Box
+            className="mb-4 p-4 bg-white rounded shadow"
+            sx={{
+              flex: 1,
+              padding: "8px 12px",
+              borderRadius: "6px",
+              cursor: "pointer",
+              "&:hover": { backgroundColor: "#e0e0e0" },
+            }}
+          >
             {/* Sección 1 */}
-          Today
+            Today
           </Box>
-          <Box className="mb-4 p-4 bg-white rounded shadow" sx={{ flex: 1 }}>
+
+          <Box
+            className="mb-4 p-4 bg-white rounded shadow"
+            sx={{
+              flex: 1,
+              padding: "8px 12px",
+              borderRadius: "6px",
+              cursor: "pointer",
+              "&:hover": { backgroundColor: "#e0e0e0" },
+            }}
+          >
             {/* Sección 2 */}
-        This week
+            This week
           </Box>
-          <Box className="mb-4 p-4 bg-white rounded shadow" sx={{ flex: 1 }}>
+
+          <Box
+            className="mb-4 p-4 bg-white rounded shadow"
+            sx={{
+              flex: 1,
+              padding: "8px 12px",
+              borderRadius: "6px",
+              cursor: "pointer",
+              "&:hover": { backgroundColor: "#e0e0e0" },
+            }}
+          >
             {/* Sección 3 */}
             This month
           </Box>
-          <Box className="p-4 bg-white rounded shadow" sx={{ flex: 5 }}>
+
+          <Box
+            className="p-4 bg-white rounded shadow"
+            sx={{
+              flex: 5,
+              padding: "8px 12px",
+              borderRadius: "6px",
+              cursor: "pointer",
+              "&:hover": { backgroundColor: "#e0e0e0" },
+            }}
+          >
             {/* Sección 4 */}
             Insights
           </Box>
         </Box>
-
 
         {/* Columna derecha */}
         <Box
@@ -40,8 +79,7 @@ export default function TaskPlanner() {
             backgroundColor: "#e0e0e0",
             padding: 2,
           }}
-        >
-        </Box>
+        ></Box>
       </Box>
     </Box>
   );
